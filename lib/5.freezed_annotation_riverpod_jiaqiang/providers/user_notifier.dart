4a9c1485b887dd1f55b742ca05889cb266fdc036
a123123 <shitlaoge@gmail.com>
2026-01-08 21:09:38 +0900

    提交代码

--- lib/5.freezed_annotation_riverpod_jiaqiang/providers/user_notifier.dart
+++ lib/5.freezed_annotation_riverpod_jiaqiang/providers/user_notifier.dart
@@ -7,12 +7,7 @@ class UserRepository {
   Future<User> getUser(int id) async {
     print("build====> getUser =$id");
     await Future.delayed(const Duration(seconds: 3)); // 模拟网络延迟
-    return User(
-      id: '$id',
-      name: 'User $id',
-      age: 20 + id,
-      avatarUrl: 'https://i.pravatar.cc/150?img=$id',
-    );
+    return User(id: '$id', name: 'User $id', age: 20 + id, avatarUrl: 'https://i.pravatar.cc/150?img=$id');
   }
 }
 
@@ -22,6 +17,8 @@ final userRepositoryProvider = Provider((ref) => UserRepository());
 class UserNotifier extends _$UserNotifier {
   bool _isDisposed = false;
 
+  // 如果你的 build 方法返回 AsyncValue<T>（而非直接 Future<T>），依然会生成 AsyncNotifierProvider，因为 AsyncValue 是 Riverpod 专门用于封装异步状态（加载 / 成功 / 错误）的类型
+  //重点： 这里带了参数userId，生成后的 Provider 会变成「带 Family 的自动销毁版本」，通过这个来实现状态隔离
   @override
   Future<User> build(int userId) async {
     print("build====>");
@@ -31,7 +28,12 @@ class UserNotifier extends _$UserNotifier {
     });
     initGetUsers();
     // 自动在 watch 时加载数据
-    return User(id: "id", name: "name", age: 100, avatarUrl: "avatarUrl"); //上来给一个默认数据，免得上来就是loading ,但是AsyncValue<User>可以控制的更好
+    return User(
+      id: "id",
+      name: "name",
+      age: 100,
+      avatarUrl: "avatarUrl",
+    ); //上来给一个默认数据，免得上来就是loading ,但是AsyncValue<User>可以控制的更好
   }
 
   //返回AsyncValue方式
@@ -40,10 +42,11 @@ class UserNotifier extends _$UserNotifier {
   //   return AsyncValue.loading();
   // }
 
-    initGetUsers() async {
-      final user =   await ref.read(userRepositoryProvider).getUser(userId);
-      state=AsyncValue.data( user);
-    }
+  initGetUsers() async {
+    final user = await ref.read(userRepositoryProvider).getUser(userId);
+    state = AsyncValue.data(user);
+  }
+
   Future<void> fetchUser(int id) async {
     // state = const AsyncValue.loading(); //触发userState.when 里面的 loading方法
     // final newState  = await AsyncValue.guard(() => ref.read(userRepositoryProvider).getUser(id));
